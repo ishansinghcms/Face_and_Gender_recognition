12ab5ec8bcff949e750d93f7a8dd5a84d5f48bef
ishansingh <ishan.1999@gmail.com>
2021-07-17 01:04:30 +0530

    Initial commit

---
+++ FaceRecognitionApp/main.py
@@ -0,0 +1,16 @@
+from logging import debug
+from flask import Flask
+import views
+
+app= Flask(__name__)
+
+#url
+app.add_url_rule('/', 'base', views.base)
+
+app.add_url_rule('/faceapp', 'faceapp', views.faceapp)
+app.add_url_rule('/faceapp/gender', 'gender', views.gender, methods=['GET', 'POST'])
+
+
+#run
+if __name__ == "__main__":
+    app.run(debug=True)
